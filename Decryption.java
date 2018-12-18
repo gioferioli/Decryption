ff594015d51e1935f8df983793615f00d98602ca
gioferioli <45973099+gioferioli@users.noreply.github.com>
2018-12-18 11:30:58 -0500

    Update Decryption.java
    added comments and made corrections to issues with converting from the counter array shifted numbers back to letters and into the string again.

--- Decryption.java
+++ Decryption.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class decryption {
 
 	public static void main(String[] args) {
+		//scans input.
 		Scanner sc = new Scanner(System.in);
 		String message = sc.nextLine();
 		String LCmessage= message.toLowerCase();
@@ -10,15 +11,20 @@ public class decryption {
 		int[] counter2 = new int[26];
 		int shift1 = 0;
 		int shift2 = 0;
-		for(int i = 0;i<LCmessage.length();i=i+2) {
+		
+		for(int i = 0;i<LCmessage.length();i+=2) {
+			// gets index of each even index in the string
 			int index1 = alphabet.indexOf(LCmessage.charAt(i));
 			if(index1!=-1) {
-			char character1 = LCmessage.charAt(i);    
+			// if the letter is in the alphabet string, then it adds it to the counter1 array
+			char character1 = LCmessage.charAt(i); 
+			//converts to ascii	
 			int ascii1 = (int)character1;
+			//counter1 array is shifted to accomadate the shift in the ascii table
 			counter1[ascii1-97]+=1;
 			}
 		}
-		
+		// same thing is done below, excpet it is with odd index instead of even.
 		for(int k = 1;k<LCmessage.length();k+=2) {
 			int index2 = alphabet.indexOf(LCmessage.charAt(k));
 			if(index2 != -1) {
@@ -27,27 +33,37 @@ public class decryption {
 			counter2[ascii2-97]+=1;
 			}
 		}
-		int efreq1= frequency1(counter1) ;
-		int efreq2= frequency2(counter2) ;
+		//function that returns the highest number in the counter slots
+		int efreq1 = frequency1(counter1) ;
+		int efreq2 = frequency2(counter2) ;
 		
 		for(int w = 0;w<LCmessage.length();w+=2) {
 			if(efreq1>4) {
 				shift1 = efreq1-4;
-		}
+		}else{
 			shift1 = 4-efreq1;
-			int t1= (int)LCmessage.charAt(w);
-			int NL2 = shift1+t1;
-			char h1 = (char)NL2;
+			}
+			int NL1 = shift1+(int)LCmessage.charAt(w);
+			if(NL1>122){
+			int FN=122-NL1;
+			}else{
+			FN = NL1;
+			}
+			char h1 = (char)FN;
 			LCmessage = LCmessage.substring(0,w)+ h1 + LCmessage.substring(w+1); 
 			}
 		for(int f = 1;f<LCmessage.length();f+=2) {
 			if(efreq2>4) {
 				shift2 = efreq2-4;
-		}
+		}else{
 			shift2 = 4-efreq2;
-			int t2= (int)LCmessage.charAt(f);
-			int NL2 = shift2+t2;
-			char h2 = (char)NL2;
+			}
+			int NL2 = shift2+(int)LCmessage.charAt(f);
+			if(NL2>122){
+			int x = 122-NL2;	
+			}else
+			x = NL2;
+			char h2 = (char)x;
 			LCmessage = LCmessage.substring(0,f)+ h2 + LCmessage.substring(f+1); 
 			}
 		System.out.print(LCmessage);
@@ -58,20 +74,19 @@ public class decryption {
 	public static int frequency1(int[] a) {
 		int k = 0;
 		for(int i = 0;i<a.length;i++) {
-				if(a[i]>k) {
-					k = i;
-				}
-				
+		   if(a[i]>k) {
+			k = i;
+	          }		
 		}
 		return k;
-	}
-	public static int frequency2(int[] a) {
+	    }
+	public static int frequency2(int[] b) {
 		int e = 0;
-		for(int i = 0;i<a.length;i++) {
-				if(a[i]>e) {
-					e = i;
-				}
+		for(int i = 0;i<b.length;i++) {
+		   if(b[i]>e) {
+			e = i;
+		  }
 		}
 		return e;
-	}
+	    }
 }
